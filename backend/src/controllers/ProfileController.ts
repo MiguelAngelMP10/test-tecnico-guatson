676b44c1052a7473ad9cffe478568f03406b132a
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-09-11 10:18:18 -0600

    se edita ProfileController.ts para poder editar y obtener los perfiles

--- backend/src/controllers/ProfileController.ts
+++ backend/src/controllers/ProfileController.ts
@@ -1,9 +1,20 @@
 import {Request, Response} from 'express';
+import {FiscalProfile} from '../models/FiscalProfile';
 
-export const getProfiles = (req: Request, res: Response) => {
-    res.json({'a': 'getProfiles'});
+export const getProfiles = async (req: Request, res: Response) => {
+    try {
+        const fiscalProfiles = await FiscalProfile.findAll();
+        res.json(fiscalProfiles);
+    } catch (error) {
+        res.status(500).json({message: 'Error al obtener FiscalProfiles', error});
+    }
 };
 
-export const createProfile = (req: Request, res: Response) => {
-    res.json({'a': 'createProfile'});
+export const createProfile = async (req: Request, res: Response) => {
+    try {
+        const fiscalProfile = await FiscalProfile.create(req.body);
+        res.status(201).json(fiscalProfile);
+    } catch (error) {
+        res.status(500).json({message: 'Error al crear FiscalProfile', error});
+    }
 };
