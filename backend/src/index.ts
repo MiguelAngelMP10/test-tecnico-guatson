6ded2ad1772aef4464562e1b042e51b91786a1ae
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-09-12 16:10:46 -0600

    se agragan cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from "./routes";
 import {connectDB} from './models';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
+// Configurar CORS de forma predeterminada (permite todas las solicitudes)
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1', routes);
 
