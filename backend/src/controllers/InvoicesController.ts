d2b82ea478861ba103e17a3fdc5d28f6d6b347e2
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-09-12 19:04:50 -0600

    update

--- backend/src/controllers/InvoicesController.ts
+++ backend/src/controllers/InvoicesController.ts
@@ -17,6 +17,7 @@ export const getInvoices = async (req: Request, res: Response) => {
             page,
             paginate: pageSize,
             order: [['createdAt', 'DESC']],
+            where: {fiscalProfileId: req.query.fiscalProfileId},
         };
 
         // @ts-ignore
